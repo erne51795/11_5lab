fa21731c9d37070897764248fc36ee2bfea59969
erne51795 <66523436+erne51795@users.noreply.github.com>
2020-08-31 22:18:17 -0500

    Add files via upload

---
+++ SakilaContext.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using System.Text;
+
+namespace _11_5Lab
+{
+    class SakilaContext : DbContext
+    {
+
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;")
+;
+        }
+    }
+}
